f824fdc8b532278634e81a31751baf342879d484
pavanagrawal123 <pavanagrawal@outlook.com>
2018-01-03 18:50:59 -0600

    read from data now rather than repetitive

--- ide/static/js/modelZoo.js
+++ ide/static/js/modelZoo.js
@@ -3,56 +3,102 @@ import ModelElement from './modelElement';
 
 class ModelZoo extends React.Component {
   render() {
-    return (
-      <div className="zoo-modal">
-        <div className="centered-zoo-modal">
-          <div className="zoo-modal-model">
-            <h3 className="zoo-modal-text">Recognition</h3>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="alexnet">AlexNet</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="All_CNN">All CNN</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="vgg16">VGG 16</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="densenet">DenseNet</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="GoogleNet">GoogLeNet</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="resnet101">ResNet 101</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="keras" id="v3">Inception V3</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="Squeezenet">Squeezenet</ModelElement>
-          </div>
-          <div className="zoo-modal-model">
-            <h3 className="zoo-modal-text">Detection</h3>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="fcn">FCN32 Pascal</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="yolo_net">YOLONet</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="HED">HED</ModelElement>
-            <h3 className="zoo-modal-text">Retrieval</h3>
-              <ModelElement importNet={this.props.importNet} framework="caffe" id="siamese_mnist">MNIST Siamese</ModelElement>
-          </div>
-          <div className="zoo-modal-model">
-            <h3 className="zoo-modal-text">Seq2Seq</h3>
-              <ModelElement importNet={this.props.importNet} framework="keras" id="textGeneration">Text Generation</ModelElement>
-              <br/>
-              <ModelElement importNet={this.props.importNet} framework="keras" id="seq2seq_lang">Seq2Seq Translation</ModelElement>
-          </div>
-          <div className="zoo-modal-model">
-            <h3 className="zoo-modal-text">Caption</h3>
-            <ModelElement importNet={this.props.importNet} framework="caffe" id="CoCo_Caption">CoCo Caption</ModelElement>
-            <h3 className="zoo-modal-text">Segmentation</h3>
-            <ModelElement importNet={this.props.importNet} framework="caffe" id="fcn2">Semantic Segmentation</ModelElement>
-          </div>
-          <div className="zoo-modal-model">
-            <h3 className="zoo-modal-text">VQA</h3>
-            <ModelElement importNet={this.props.importNet} framework="keras" id="VQA">VQA</ModelElement>
-          </div>
-        </div>
+    var toDisplay = [];
+    var zooData =  {
+        'Recognition': [{
+          'id': 'alexnet',
+          'framework': 'caffe',
+          'name': 'AlexNet'
+        }, {
+          'id': 'All_CNN',
+          'framework': 'caffe',
+          'name': 'All CNN'
+        }, {
+          'id': 'vgg16',
+          'framework': 'caffe',
+          'name': 'VGG 16'
+        }, {
+          'id': 'densenet',
+          'framework': 'caffe',
+          'name': 'DenseNet'
+        }, {
+          'id': 'GoogleNet',
+          'framework': 'caffe',
+          'name': 'GoogLeNet'
+        }, {
+          'id': 'resnet101',
+          'framework': 'caffe',
+          'name': 'ResNet 101'
+        }, {
+          'id': 'v3',
+          'framework': 'keras',
+          'name': 'Inception V3'
+        }, {
+          'id': 'Squeezenet',
+          'framework': 'caffe',
+          'name': 'Squeezenet'
+        }],
+        'Detection': [{
+          'id': 'fcn',
+          'framework': 'caffe',
+          'name': 'FCN32 Pascal'
+        }, {
+          'id': 'yolo_net',
+          'framework': 'caffe',
+          'name': 'YOLONet'
+        }, {
+          'id': 'HED',
+          'framework': 'caffe',
+          'name': 'HED'
+        }, {
+          'id': 'siamese_mnist',
+          'framework': 'caffe',
+          'name': 'MNIST Siamese'
+        }],
+        'Seq2Seq': [{
+          'id': 'textGeneration',
+          'framework': 'keras',
+          'name': 'Text Generation'
+        }, {
+          'id': 'seq2seq_lang',
+          'framework': 'keras',
+          'name': 'Seq2Seq Translation'
+        }],
+        'Caption': [{
+          'id': 'CoCo_Caption',
+          'framework': 'caffe',
+          'name': 'CoCo Caption'
+        }, {
+          'id': 'fcn2',
+          'framework': 'caffe',
+          'name': 'Semantic Segmentation'
+        }],
+        'VQA': [{
+          'id': 'VQA',
+          'framework': 'keras',
+          'name': 'VQA'
+        }]
+      }
+    Object.keys(zooData).map(category =>
+      toDisplay.push(
+      <div key={category} className="zoo-modal-model">
+          <h3 className="zoo-modal-text">{category}</h3>
+            {Object.keys(zooData[category]).map(modelEl =>
+              <div key={zooData[category][modelEl]['id']}>
+              <ModelElement importNet={this.props.importNet} 
+              framework={zooData[category][modelEl]['framework']} 
+              id={zooData[category][modelEl]['id']}> 
+              {zooData[category][modelEl]['name']} 
+              </ModelElement>
+              <br></br>
+              </div>
+            )}
       </div>
-    );
+      )
+    )
+    return (
+      <div className="zoo-modal"><div className="centered-zoo-modal"> {toDisplay} </div></div>
+      )
   }
 }
 
